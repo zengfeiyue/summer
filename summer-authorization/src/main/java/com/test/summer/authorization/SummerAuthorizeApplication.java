514b4b098d579ec3873659be913d3b743650976c
zengfeiyue <770144935@qq.com>
2018-04-26 09:00:49 +0800

    init commit

---
+++ summer-authorization/src/main/java/com/test/summer/authorization/SummerAuthorizeApplication.java
@@ -0,0 +1,12 @@
+package com.test.summer.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerAuthorizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerAuthorizeApplication.class, args);
+	}
+}
