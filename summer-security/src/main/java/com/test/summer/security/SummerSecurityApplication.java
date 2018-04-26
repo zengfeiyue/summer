eae637ebe73cb0c13a45f34ec1724036e3e7b7ef
zengfeiyue <770144935@qq.com>
2018-04-26 08:28:27 +0800

    init commit

---
+++ summer-security/src/main/java/com/test/summer/security/SummerSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.test.summer.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerSecurityApplication.class, args);
+	}
+}
