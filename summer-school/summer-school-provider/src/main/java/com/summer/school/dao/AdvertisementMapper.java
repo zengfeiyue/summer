f3d51030f6ed3c8f198fb3c6da73b71764a404f8
zengfeiyue <770144935@qq.com>
2018-08-20 10:58:21 +0800

    橫幅幻燈片

---
+++ summer-school/summer-school-provider/src/main/java/com/summer/school/dao/AdvertisementMapper.java
@@ -0,0 +1,8 @@
+package com.summer.school.dao;
+
+import com.summer.common.base.BaseMapper;
+import com.summer.school.api.entity.Advertisement;
+
+public interface AdvertisementMapper extends BaseMapper<Advertisement,Integer> {
+
+}
